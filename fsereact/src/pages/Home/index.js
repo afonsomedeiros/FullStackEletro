9228ead1cb107747fd2b90830afff8d38deebcfd
Afonso Benjamin e Silva Medeiros <afonso.b.medeiros@gmail.com>
2020-11-25 10:04:12 -0300

    Ajustes.

--- fsereact/src/pages/Home/index.js
+++ fsereact/src/pages/Home/index.js
@@ -5,7 +5,7 @@ export default function Home(props) {
         <Row>
             <div className="col-lg-6 col-sm-12 mx-auto">
                 <h3><b>Seja bem vindo (a)!</b></h3>
-                <p class="lead">
+                <p className="lead">
                     Aqui em nossa loja, <em><strong>programadores tem desconto</strong></em> nos produtos para sua casa!
                 </p>
             </div>
